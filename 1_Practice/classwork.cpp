766a9428e5f6a6e5f77264451704777f0c5e8b91
Niruja Bhandari <nirujab12@gmail.com>
2023-06-05 09:15:34 +0545

    classwork

---
+++ 1_Practice/classwork.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+class sathiharu {
+    public: 
+
+    sathiharu(string nam,int umer,string addr) {
+    
+    
+            cout<<"\nOi Espali ghumna jaam bujhis "<<nam<<" umm "
+            <<umer<<"ko umer ma hike garna sakinchha masta. ka jane hola ta "
+            <<addr<<" jada thik hola?";
+        }
+};
+
+int main() {
+    sathiharu firstfriend("Shruti",19,"Godawari"), secondfriend("Pabitra",20,"Godawari");
+    
+
+}
